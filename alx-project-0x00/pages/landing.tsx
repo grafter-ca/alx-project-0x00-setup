33dda55946d7f6daf71ea1a0bb9f4dc8cb1526cd
grafter-ca <caleb.designer1@gmail.com>
2025-07-15 20:04:38 +0200

    adding Button comp with p, buttonprops interface and render it in landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,15 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Card />
+
+      <Button   title="Book Now/small" styles="px-2 py-1 border-2 mr-2 rounded-sm"/>
+      <Button   title="Book Now/medium" styles="px-2 py-1 border-2 mr-2 rounded-md"/>
+      <Button   title="Book Now/large" styles="px-2 py-1 border-2 mr-2 rounded-full"/>
     </div>
   )
 }
