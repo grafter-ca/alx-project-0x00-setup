159a123f42c94b222b8f74d1013cf8683a0d357f
grafter-ca <caleb.designer1@gmail.com>
2025-07-15 20:11:02 +0200

    checking button styles, sm,md,lg and full

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,10 +6,10 @@ const Landing: React.FC =  () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-
       <Button   title="Book Now/small" styles="px-2 py-1 border-2 mr-2 rounded-sm"/>
       <Button   title="Book Now/medium" styles="px-2 py-1 border-2 mr-2 rounded-md"/>
       <Button   title="Book Now/large" styles="px-2 py-1 border-2 mr-2 rounded-lg"/>
+      <Button   title="Book Now/full" styles="px-2 py-1 border-2 mr-2 rounded-full"/>
     </div>
   )
 }
